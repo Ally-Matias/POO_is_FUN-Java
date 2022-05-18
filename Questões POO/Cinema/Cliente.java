a8c875746f76c9d7d8c11adfefa7354f92cb6e20
Alliquison Matias <allyquison.matias@gmail.com>
2022-05-18 16:55:14 -0300

    Create Cliente.java

---
+++ Questões POO/Cinema/Cliente.java
@@ -0,0 +1,29 @@
+class Client {
+    private String id;
+    private String fone;
+
+    public Client(String id, String fone) {
+        this.id = id;
+        this.fone = fone;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String toString() {
+        return String.format("%s:%s", getId(), getFone());
+    }
+}
