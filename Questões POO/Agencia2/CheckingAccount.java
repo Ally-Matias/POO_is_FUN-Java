833085895ba3ffb5c5e35fb52ea5a28ea33a41a6
Alliquison Matias <allyquison.matias@gmail.com>
2022-06-13 20:07:02 -0300

    Create CheckingAccount.java

---
+++ Questões POO/Agencia2/CheckingAccount.java
@@ -0,0 +1,14 @@
+package atividades.Agencia2;
+
+public class CheckingAccount extends Account{
+	//inicializa conta.type com "CC"
+    public CheckingAccount(int id, String idClient) {
+    	super(id,idClient);
+    	super.type = "CC";
+    }
+    //retira 20 do saldo
+    public void monthlyUpdate() {
+    	super.balance -= 20;
+    }
+
+}
